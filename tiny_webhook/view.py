774f82888f01ff3572d81c13585ef2c2d892d013
Andre Geraseev <ageraseev@gmail.com>
2023-12-06 13:50:23 -0300

    outras coisas tiny

--- tiny_webhook/view.py
+++ tiny_webhook/view.py
@@ -5,7 +5,7 @@ from tiny_webhook.serializers import ProductSerializer
 class ProductWebhook(APIView):
     def post(self, request, format=None):
         print(request.data['dados'])
-        serializer = ProductSerializer(data=request.data.dados, context={'request': request})
+        serializer = ProductSerializer(data=request.data['dados'], context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=201)
