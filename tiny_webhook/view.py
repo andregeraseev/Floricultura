f644832357886c06a6985a9022b8bd1bd4a88f23
Andre Geraseev <ageraseev@gmail.com>
2023-12-06 15:19:47 -0300

    outras coisas tiny

--- tiny_webhook/view.py
+++ tiny_webhook/view.py
@@ -16,6 +16,7 @@ class ProductWebhook(APIView):
         serializer = ProductSerializer(data=payload['dados'], context={'request': payload['dados']})
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=201)
+            print(serializer.data)
+            return Response(serializer.data, content_type="application/json", status=200 )
         else:
             return Response(serializer.errors, status=400)
