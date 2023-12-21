cc59d835411b66bd8f0722753c7587b11bb7bc63
Andre Geraseev <ageraseev@gmail.com>
2023-12-21 14:10:57 -0300

    mobili e checkpit

--- pedidos/forms.py
+++ pedidos/forms.py
@@ -79,7 +79,9 @@ class ComprovanteForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(ComprovanteForm, self).__init__(*args, **kwargs)
         self.fields['comprovante'].required = True
-
+        self.fields['comprovante'].label = 'Comprovante de Pagamento'
+        self.fields['comprovante'].help_text = 'Envie o comprovante de pagamento para que possamos identificar seu pedido.'
+        self.fields['comprovante'].widget.attrs.update({'capture': 'environment'})
     class Meta:
         model = Order
         fields = ['comprovante']
